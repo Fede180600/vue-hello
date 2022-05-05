4aafef868dbecc73cd859d484be10b1c31c1f1bc
Fede180600 <fede180600@libero.it>
2022-05-05 14:23:54 +0200

    create new Vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'Questo è un contenuto inserito dinamicamente con Vuejs'
+        }
+    }
+)
